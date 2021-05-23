363348b0b7262e032ade25e92eb1ef4aef9e4f1f
Josh Vanderwillik <josh@willik.ca>
2021-05-23 10:05:13 -0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -5,16 +5,27 @@ const control_panel = require('./control-panel')
 const shout = require('./shout')
 const log = require('./util/log')
 
-let app = express()
-let tools = {
-  quote: {title: 'Pointless quote', path: '/quote'},
-  cp: {title: 'Pointless control panel', path: '/cp'},
-  shout: {disabled: true, title: 'Pointless shouting email address', path: '/shout'},
-}
-let register_tool = (opt) => {
+let tools = {}
+let register_tool = (key, opt) => {
+  opt.name = opt.name || key
+  opt.title = opt.title || `Pointless ${opt.name}`
+  opt.path = opt.path || `/${key}`
+  opt.handler = typeof opt.handler == `string` ? require(opt.handler) :
+    opt.handler
   tools[opt.name] = opt
-  opt.title = opt.name[0].toUpperCase() + opt.name.slice(1)
 }
+register_tool('quote', {
+  handler: './tools/quote.js',
+})
+register_tool('cp', {
+  name: 'control panel',
+  handler: control_panel,
+})
+register_tool('shout', {
+  name: 'shouting email address',
+  handler: shout.routes(),
+  disabled: true,
+})
 let tool_index = (req, res, next) => {
   let html = '<h1>Pointless Services</h1>'
   +_(tools)
@@ -25,11 +36,15 @@ let tool_index = (req, res, next) => {
   res.set('Content-Type', 'text/html')
   res.send(html)
 }
+let app = express()
 app.use(control_panel.site_status)
 app.get('/', tool_index)
-app.get(tools.quote.path, require(`./tools/quote`))
-app.use(tools.cp.path, control_panel)
-app.use(tools.shout.path, shout.routes())
+for (let tool of Object.values(tools)) {
+  if (tool.disabled) {
+    continue
+  }
+  app.use(tool.path, tool.handler)
+}
 app.listen(config.port, () => {
   console.log(`Listening on :${config.port}`)
   shout.init().catch(log.error)
